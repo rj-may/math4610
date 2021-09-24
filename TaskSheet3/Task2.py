2982a9a666d794f263afb6c3097e80dabe8b68f1
rj-may <89875220+rj-may@users.noreply.github.com>
2021-09-23 21:24:54 -0600

    Update Task2.py

--- TaskSheet3/Task2.py
+++ TaskSheet3/Task2.py
@@ -2,4 +2,28 @@ import matplotlib.pyplot as plt
 
 
 #import stuff from Task1 
+xVal = 2
+
+def secDerivApprox(x, h):
+    approximation = (cos(x + h) - 2 * cos(x) + cos(x - h)) / (h **  2)
+    return approximation
+
+
+
+def main():
+    actual = -cos(xVal)
+    hVal = []
+    diff = []
+    print(format("h", "<6") + format("\t -cos(2)", "<25") + format("Central diff Approx ", "<24") + format("Difference", "<20"))
+    for i in range(-1,17):
+        if i == -1:
+            h = 1
+            approx = secDerivApprox(xVal, h)
+        elif i == 0:
+            h = 0.5
+            approx = secDerivApprox(xVal, h)
+        else:
+            h = 1 * (10 ** (-1 * i))
+            approx = secDerivApprox(xVal, h)
+        hVal.append(h)
 

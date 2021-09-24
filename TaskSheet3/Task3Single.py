55ed60e2e2e9284b1ebaa967529bf1ddbf621706
rj-may <rj.may@hotmail.com>
2021-09-24 17:28:14 -0600

    Ready to rock

--- TaskSheet3/Task3Single.py
+++ TaskSheet3/Task3Single.py
@@ -1,15 +1,20 @@
 #Single precision of of machine epislon
+'''
+important note about using numpy sinlge number; when peforming operations on 2 different things 
+you have to make sure both are the value that you want. It will default between the larger bit value
+Ex : single * double will result with a double. Had to read documentation. 
+'''
 import numpy as np
 
 def singlePrecision():
-    x = 1
-    eps = (1/2) #epsilon
+    x = np.single(1)
+    eps = np.single(1/2) #epsilon
     for i in range(40):
         xApprox = x + eps
         error = abs(x - xApprox)
         if error == 0:
             break
-        eps = np.finfo(np.single).eps
+        eps = np.single(eps / 2)
         print(i, error)
 
 def main():

687f4c941b2a03fff14b8cc42b617f63e17dde23
rj-may <89875220+rj-may@users.noreply.github.com>
2021-09-23 23:13:23 -0600

    Update Task3Single.py
    Single precision added

--- TaskSheet3/Task3Single.py
+++ TaskSheet3/Task3Single.py
@@ -1,2 +1,20 @@
 #Single precision of of machine epislon
+import numpy as np
 
+def doublePrecision():
+    x = 1
+    eps = 1/2 #epsilon
+    for i in range(100):
+        xApprox = x + eps
+        error = abs(x - xApprox)
+        if error == 0:
+            break
+        eps = eps / 2
+        eps = np.float32(eps)
+        print(i, error)
+
+def main():
+    doublePrecision()
+
+
+main()

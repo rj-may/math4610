c9048e7ac483340daa8ae28fda9686d13664cf94
rj-may <rj.may@hotmail.com>
2021-09-24 18:27:23 -0600

    magic

---
+++ lib/precision/doubleMachEps.py
@@ -0,0 +1,17 @@
+# here is the double precision  part of the project
+# This is to test the double precision part of the project
+import numpy as np
+
+def doublePrecision():
+    print("Iteration, machine epsilon")
+    x = 1
+    eps = 1/2 #epsilon
+    for i in range(100):
+        xApprox = x + eps
+        error = abs(x - xApprox)
+        if error == 0:
+            break
+        eps = np.float64(eps /2)
+        print(i, error)
+
+
---
+++ lib/precision/singleMachEps.py
@@ -0,0 +1,20 @@
+#Single precision of of machine epislon
+'''
+important note about using numpy sinlge number; when peforming operations on 2 different things 
+you have to make sure both are the value that you want. It will default between the larger bit value
+Ex : single * double will result with a double. Had to read documentation. 
+'''
+import numpy as np
+
+def singlePrecision():
+    print("Iteration, machine epsilon")
+    x = np.single(1)
+    eps = np.single(1/2) #epsilon
+    for i in range(40):
+        xApprox = x + eps
+        error = abs(x - xApprox)
+        if error == 0:
+            break
+        eps = np.single(eps / 2)
+        print(i, error)
+

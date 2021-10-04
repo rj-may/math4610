98c5bf872d4020d1cee6a185c60b6b23825c1925
rj-may <rj.may@hotmail.com>
2021-10-03 20:13:29 -0600

    Task1.py

---
+++ TaskSheet4/Task1.py
@@ -0,0 +1,19 @@
+#x is the exact value
+#y is the approximation 
+
+
+
+def absErr(x, y):
+	return abs(x-y)
+
+def relErr(x, y):
+	return abs((x-y)/x)
+
+
+
+#def test():
+    print(absErr(4, 5))
+    print(relErr(4,5))
+
+#test()
+

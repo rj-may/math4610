3c92142444d34259e3c66b1d96fdfed2075c89ab
rj-may <rj.may@hotmail.com>
2021-10-04 10:20:05 -0600

    Playing around

--- TaskSheet4/Task3.py
+++ TaskSheet4/Task3.py
@@ -1,12 +1,13 @@
-# Here is the code for a fixed point iteration.
+# Here is the code for a fixed point iteratioin.
 
-# fx is funciton of x, maxIter is max iterations
+# fx is funciton of x, tol is for the tolerance,  maxIter is max iterations
 def fixedPoint(x0, fx, tol, maxIter):
     iter = 0
     error = 10 * tol
     while(error > tol and iter < maxIter):
-        x1 = fx(x0)
+        x1 = x - fx(x0)
         error = abs(x1 - x0)
         x0 = x1
         iter += 1
+        # print(iter)
     return x1
--- TaskSheet4/Task4.py
+++ TaskSheet4/Task4.py
@@ -6,7 +6,7 @@ import Task3
 fixPtIter = Task3.fixedPoint
 
 def funkyFunc(x):
-    return (x * math.exp(3 * (x ** 2))) - (7 * x)
+    return ( (x * math.exp(3 * x * x)) - (7 * x) )
 
 
 def main():
@@ -14,7 +14,7 @@ def main():
     tol = .1
     maxIter = 20 
     ans = fixPtIter(start, funkyFunc, tol, maxIter)
-    print("This function started at x= ", start, ". The tolerance is ", tol, ". The max iterations was ", maxIter, ". ")
+    print("This function started at x= ", start, ". The tolerance was ", tol, ". The max iterations was ", maxIter, ". ")
     print("The first root found was", ans)
 
 

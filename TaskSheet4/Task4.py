2343036d39ef13a6821085d0acbd8576e44f0a2b
rj-may <rj.may@hotmail.com>
2021-10-05 23:00:26 -0600

    Done with 4

--- TaskSheet4/Task4.py
+++ TaskSheet4/Task4.py
@@ -5,20 +5,25 @@ import Task3
 
 fixPtIter = Task3.fixedPoint
 
+#this funcition breaks because of math overflow  eror. 
 def funkyFunc(x):
     return ( (x * math.exp(3 * x * x)) - (7 * x) )
 
-
-def epsFunc(x):
+#here is the value of epsilon times the f(x) or funkyFunc. 
+# epsilon was determined to be 2 / f'(x).  This was determined by solving (g'(x)) <= 1 where g(x) = x - f(x)
+def epsFunc(x): 
     return  2 /(math.exp(3 * x * x) + 6 * x * x * math.exp(3 * x * x)-7)   * (x * math.exp(3 * x * x) - (7 * x))
 
+def run(start, tolerance):
+    maxIter = 200 
+    ans = fixPtIter(start, epsFunc, tolerance, maxIter)
+    print("This function started at x= ", start, ". The tolerance was ", tolerance, ". The max iterations was ", maxIter, ". ")
+    print("The root found was", ans)
+
 def main():
-    start = .8 
-    tol = .01
-    maxIter = 100 
-    ans = fixPtIter(start, epsFunc, tol, maxIter)
-    print("This function started at x= ", start, ". The tolerance was ", tol, ". The max iterations was ", maxIter, ". ")
-    print("The first root found was", ans)
+	run(0, 0.02)
+	run(.5, 0.02)
+	run(.8, 0.01)
 
 
 main()

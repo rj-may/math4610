5de5cf660159a53980725897528e66a568373a89
rj-may <rj.may@hotmail.com>
2021-10-12 21:39:32 -0600

    Newton and secant done

---
+++ TaskSheet5/Task1.py
@@ -0,0 +1,34 @@
+
+
+def newtonRoot(func, df, x0, maxIter, err):
+    if abs(func(x0)) < err: 
+        return x0
+    xNext = x0
+    for i in range(maxIter):
+        xNext = x0 - func(x0)/df(x0)
+        if abs(xNext - x0) < err:
+            return xNext
+        x0 = xNext
+
+    print("Max iterations reached. ")
+
+
+
+def func(x):
+    return ( (x * math.exp(3 * x * x)) - (7 * x) )
+
+def df(x):
+    return (math.exp(3 * x * x) + 6 * x * x * math.exp(3 * x * x)-7)
+
+
+def main():
+    x1 = .7
+    error = .001
+    maxIter = 80
+    print("Funciton with starting x = %f with an error of %f and a max number of iterations of %d" %(x1, error, maxIter))
+    root = newtonRoot(func, df, x1, maxIter, error)
+    print("Root: ", root)
+    
+main()
+
+    
---
+++ lib/secantRoot.py
@@ -0,0 +1,30 @@
+import math
+
+def secantRoot(func, x0, x1, maxIter, err):
+    if abs(func(x0)) < err: 
+        return x0
+    for i in range(maxIter):
+        x2 = x1 - func(x1) * (x1 - x0)/ (func(x1)- func(x0))
+        if abs(x2 - x1) < err:
+            return x2
+        x0, x1 = x1, x2 # reassigning variables. 
+
+    print("Max iterations reached. ")
+
+
+# def func(x):
+#     return ( (x * math.exp(3 * x * x)) - (7 * x) )
+
+
+# def main():
+#     x0 = .6
+#     x1 = .9
+#     error = .001
+#     maxIter = 80
+#     print("Funciton with starting x0 = %f, x1 = %f, with an error of %f and a max number of iterations of %d" %(x0, x1, error, maxIter))
+#     root = secantRoot(func, x0, x1, maxIter, error)
+#     print("Root: ", root)
+    
+# main()
+
+    
